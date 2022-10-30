f96f3c15c0ed030a17c32d5468f32ec89d0edaea
kiriakide <kiriakigew20@gmail.com>
2022-10-30 23:43:59 +0200

    hi

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import streamlit as st
 from panel.interact import interact
 import hvplot.pandas
 
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', option)
+
 stocks = vds.stocks()
 stocks.head()
 
