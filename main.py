3b57867eec24009ac2e58289da84c842ac4871ae
kiriakide <kiriakigew20@gmail.com>
2022-10-30 23:40:29 +0200

    hi

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-import streamlit as st
-
-st.set_page_config(page_title="Health Coach", page_icon=":heart:", layout="wide" )
-
-with st.container():
-    st.title ("HealaaathCoach :heart: :running:")
-    st.subheader("hey")
-
-st.sidebar.success("select a page above")
-
 import panel as pn
 import streamlit as st
 from panel.interact import interact
 pn.extension()
-
 import hvplot.pandas
-
 
 stocks = vds.stocks()
 stocks.head()
-st.write(stocks)
-st.print(stocks)
 
 symbols = list(stocks.symbol.unique())
 st.write(symbols)
+
+select = pn.widgets.Select(name='Select', options=symbols)
+
+# create function
+def create_plot(symbol):
+    return stocks[stocks['symbol'] == symbol].hvplot('date', 'price')
+
+# create interaction between widget and function
+interact(create_plot, symbol=select)
