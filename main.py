c88fb35d07668e30b0b8ec1de5d436493034ee9d
kiriakide <kiriakigew20@gmail.com>
2022-10-30 23:35:29 +0200

    hi

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="Health Coach", page_icon=":heart:", layout="wide" )
+
+with st.container():
+    st.title ("HealaaathCoach :heart: :running:")
+    st.subheader("hey")
+
+st.sidebar.success("select a page above")
+
 import panel as pn
 import streamlit as st
 from panel.interact import interact
