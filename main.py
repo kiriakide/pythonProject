dd85f31b04514b4d275ca5d2a5d9d8342d995585
kiriakide <kiriakigew20@gmail.com>
2022-10-30 23:29:57 +0200

    hi

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 import panel as pn
+import streamlit as st
+from panel.interact import interact
+pn.extension()
+
+import hvplot.pandas
+
+import holoviews as hv
+from holoviews import opts
+hv.extension('bokeh')
+
+import geoviews as gv
+gv.extension('bokeh')
+
+from vega_datasets import data as vds
+from pydataset import data as pyds
+
+import yfinance
+
+stocks = vds.stocks()
+stocks.head()
+st.(stocks)
