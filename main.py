6e41db740db283a72c55e332c3d2ffc38ef70633
kiriakide <kiriakigew20@gmail.com>
2022-10-30 23:50:32 +0200

    hi

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-
 import streamlit as st
+import yfinance as yf
+import pandas as pd
 
 option = st.selectbox(
     'How would you like to be contacted?',
     ('Email', 'Home phone', 'Mobile phone'))
 
 st.write('You selected:', option)
+
+tickerSymbol = 'GOOGL'
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
